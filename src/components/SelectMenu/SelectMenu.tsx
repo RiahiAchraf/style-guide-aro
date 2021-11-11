f4cadc336bdc7f3e05074fffbca02b529749223a
RiahiAchraf <riahiachraf1@gmail.com>
2021-11-11 18:17:51 +0000

    pdating size-limit/file version

---
+++ src/components/SelectMenu/SelectMenu.tsx
@@ -0,0 +1,29 @@
+import React, {ReactNode} from "react";
+
+export interface ButtonProps {
+  children?:ReactNode;
+  onClick: () => void;
+}
+
+const Button: React.FC<ButtonProps> = (props) => {
+  const {
+    children = "Button Text",
+    onClick,
+  } = props;
+
+  return (
+    <button
+      type="button"
+      onClick={onClick}
+    >
+      {children}
+    </button>
+  )
+
+}
+
+
+
+
+
+export default Button;
