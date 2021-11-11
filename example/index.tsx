86fa2b0283fbe5410f2b749f322d0f97f903221c
RiahiAchraf <riahiachraf1@gmail.com>
2021-11-11 17:39:49 +0000

    Deleting lint, and test script

--- example/index.tsx
+++ example/index.tsx
@@ -2,18 +2,20 @@ import 'react-app-polyfill/ie11';
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 
-import { ToggleSwitch, TextInput } from 'style-guide-aro';
+import { ToggleSwitch, TextInput, Button } from 'style-guide-aro';
 import 'style-guide-aro/dist/tailwind.css';
 
 const App = () => {
   const [isToggled, setIsToggled] = React.useState(false);
+  const [value, setValue] = React.useState("AA")
   return (
     <div>
       <ToggleSwitch
         enabled={isToggled}
         onChange={(v: boolean) => setIsToggled(v)}
       />
-      <TextInput name="email" label="label" error optional/>
+      <TextInput value={value} name="email" label="label" error optional/>
+      <Button  />
     </div>
   );
 };
---
+++ lintError.js
@@ -0,0 +1,10 @@
+// "lint": "tsdx lint",
+// "test": "tsdx test --passWithNoTests",
+
+// name: Build, lint, and test on Node ${{ matrix.node }} and ${{ matrix.os }}
+
+// - name: Lint
+// run: yarn lint
+
+// - name: Test
+// run: yarn test --ci --coverage --maxWorkers=2
